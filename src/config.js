9875e39a686c476f1b7e746661234f107788bde9
Mathew Murray <the.thran@gmail.com>
2020-10-13 15:55:35 -0400

    auth route

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-}
+  DB_URL: process.env.DB_URL
+  || 'postgresql://dunder_mifflin@localhost/zippal',
+  JWT_SECRET: process.env.JWT_SECRET || 'change_this_secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+};
