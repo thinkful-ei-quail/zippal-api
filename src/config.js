3382930f14d977dcbdf1d0b43efbff0d782e6cf7
Mathew Murray <the.thran@gmail.com>
2020-10-13 16:24:11 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
-  || 'postgresql://dunder_mifflin@localhost/zippal',
-  JWT_SECRET: process.env.JWT_SECRET || 'change_this_secret',
+  DATABASE_URL: process.env.DB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
