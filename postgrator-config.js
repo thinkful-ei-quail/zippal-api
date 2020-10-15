8c80c7c9e199aa4a15a381a5dce3cf903f81448c
Lip Cowan <pcowan90@gmail.com>
2020-10-15 13:38:37 -0700

    prepping for migrations to heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DATABASE_HOST,
-  "port": process.env.MIGRATION_DATABASE_PORT,
-  "database": process.env.NODE_ENV === 'test' ? process.env.MIGRATION_TEST_DB_NAME : process.env.MIGRATION_DATABASE_NAME,
-  "username": process.env.MIGRATION_DATABASE_USERNAME
+  // "host": process.env.MIGRATION_DATABASE_HOST,
+  // "port": process.env.MIGRATION_DATABASE_PORT,
+  "connectionString": process.env.DATABASE_URI,
+  // "database": process.env.NODE_ENV === 'test' ? process.env.MIGRATION_TEST_DB_NAME : process.env.MIGRATION_DATABASE_NAME,
+  // "username": process.env.MIGRATION_DATABASE_USERNAME
 }
