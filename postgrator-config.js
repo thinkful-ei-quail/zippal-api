5f48160c7cbf62a8a023ad93c5f2910b8261e97f
Lip Cowan <pcowan90@gmail.com>
2020-10-19 07:28:43 -0700

    updated postgrator for local dev, pushed everything to heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,8 +5,8 @@ module.exports = {
   "driver": "pg",
   "host": process.env.MIGRATION_DATABASE_HOST,
   "port": process.env.MIGRATION_DATABASE_PORT,
-  "connectionString": process.env.DATABASE_URL,  
-  // "database": process.env.NODE_ENV === 'test' ? process.env.MIGRATION_TEST_DB_NAME : process.env.MIGRATION_DATABASE_NAME,
-  // "username": process.env.MIGRATION_DATABASE_USERNAME,
-  // "password": process.env.MIGRATION_DATABASE_PASSWORD
+  // "connectionString": process.env.DATABASE_URL,  - only used for heroku, not local
+  "database": process.env.NODE_ENV === 'test' ? process.env.MIGRATION_TEST_DB_NAME : process.env.MIGRATION_DATABASE_NAME,
+  "username": process.env.MIGRATION_DATABASE_USERNAME,
+  "password": process.env.MIGRATION_DATABASE_PASSWORD
 }
